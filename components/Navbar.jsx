a5d876044de61c3b2aeae4f0b21f8daac598118c
aliciachoi <aliciachoi2022@gmail.com>
2025-06-27 17:00:31 +0100

    Fix Navbar JSX syntax by wrapping returned elements in React fragment

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -46,30 +46,16 @@ const Navbar = () => {
   }, []);
 
   return (
-    <div
-      style={{ backgroundColor: `${navBg}` }}
-      className={
-        shadow
-          ? 'fixed w-full h-20 shadow-xl z-[100] ease-in-out duration-300'
-          : 'fixed w-full h-20 z-[100]'
-      }
-    >
-      <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-<<<<<<< HEAD
-=======
-        <Link href='/'>
-          <a>
-            <Image
-              src={NavLogo}
-              alt='/'
-              width='125'
-              height='50'
-              className='cursor-pointer'
-            />
-          </a>
-        </Link>
->>>>>>> 1133c69703104e4730e9799bb32409f560338a5b
-        <div>
+    <>
+      <div
+        style={{ backgroundColor: `${navBg}` }}
+        className={
+          shadow
+            ? 'fixed w-full h-20 shadow-xl z-[100] ease-in-out duration-300'
+            : 'fixed w-full h-20 z-[100]'
+        }
+      >
+        <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
           <ul style={{ color: `${linkColor}` }} className='hidden md:flex'>
             <li className='ml-10 text-sm uppercase hover:border-b'>
               <Link href='/'>Home</Link>
@@ -218,7 +204,7 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
